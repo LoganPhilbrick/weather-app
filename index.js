d0147da2ee91a3b8fa4f7e8538c22dd0945a0055
Trevor <txp2308@yum.com>
2022-09-12 14:53:40 -0700

    updated function

--- index.js
+++ index.js
@@ -2,11 +2,17 @@ const APIKEY = "1e92dfdcded69a53ecb95b2e92c583a2"
 const lat = "40.2415"
 const long = "-75.2838"
 
+// Function getting user's location info and fetching from the weather api
+
 const fetchWeatherData = async (APIKEY, lat, long) => {
-const coords = await  navigator.geolocation.getCurrentPosition((position) => console.log(position))
     fetch(`https://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${long}&appid=${APIKEY}`)
     .then((res) => res.json())
     .then((data) => console.log(data))
 }
 
-fetchWeatherData(APIKEY, lat, long)
+const getCoordsAndFetchData = async () => {
+   await  navigator.geolocation.getCurrentPosition(({coords}) => fetchWeatherData(APIKEY, coords.latitude, coords.longitude))
+
+}
+
+getCoordsAndFetchData()
