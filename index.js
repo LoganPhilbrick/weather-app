a8a7850ce70e82fc7f1baa9646eabfedc609e4be
LoganPhilbrick <loganphilbrick99@gmail.com>
2022-09-13 14:53:30 -0400

    created Kelvin conversion function

--- index.js
+++ index.js
@@ -1,18 +1,21 @@
 const APIKEY = "1e92dfdcded69a53ecb95b2e92c583a2"
-const lat = "40.2415"
-const long = "-75.2838"
+
 
 // Function getting user's location info and fetching from the weather api
 
 const fetchWeatherData = async (APIKEY, lat, long) => {
-    fetch(`https://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${long}&appid=${APIKEY}`)
-    .then((res) => res.json())
-    .then((data) => console.log(data))
+    fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${long}&appid=${APIKEY}`)
+        .then((res) => res.json())
+        .then((data) => console.log(data))
 }
 
-const getCoordsAndFetchData = async () => {
-   await  navigator.geolocation.getCurrentPosition(({coords}) => fetchWeatherData(APIKEY, coords.latitude, coords.longitude))
+const getCoordsAndFetchData = () => {
+    navigator.geolocation.getCurrentPosition(({ coords }) => fetchWeatherData(APIKEY, coords.latitude, coords.longitude))
 
 }
-
 getCoordsAndFetchData()
+
+
+const convertKelvin = (kelvin) => {
+    return (((kelvin - 273.15) * 1.8) + 32)
+}
