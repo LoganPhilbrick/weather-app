0209eecb66d076fd842e2ee90e6991b2453b4142
Trevor <txp2308@yum.com>
2022-09-17 17:53:43 -0700

    created date formatter

--- index.js
+++ index.js
@@ -5,23 +5,60 @@ const humidity = document.getElementById("humidity");
 const wind = document.getElementById("wind");
 const pressure = document.getElementById("pressure");
 
-// calculate date
-
-let today = new Date();
-let dd = today.getDate();
-
-let mm = today.getMonth() + 1;
-let yyyy = today.getFullYear();
-if (dd < 10) {
-  dd = "0" + dd;
-}
-
-if (mm < 10) {
-  mm = "0" + mm;
-}
-today = mm + "-" + dd + "-" + yyyy;
-
+let formattedDate;
 let weatherInfo;
+
+
+// format date
+
+const formatDate = () => {
+//create Date
+let today = new Date();
+//extract day
+let dd = today.getDate();
+//extract month
+let mm = today.getMonth() + 1;
+
+//convert date to proper full month
+switch(mm){
+  case 1:
+    mm = "January";
+  case 2:
+    mm = "february";
+  case 3:
+    mm = "March";
+  case 4:
+    mm = "April";
+  case 5:
+    mm = "May";
+  case 6:
+    mm = "June";
+  case 7:
+    mm = "July";
+  case 8:
+    mm = "August";
+  case 9:
+    mm = "September";
+  case 10:
+    mm = "October";
+  case 11:
+    mm = "November";
+  case 12: 
+  mm = "December";
+}
+
+// set string of month and day
+return formattedDate = `${mm} ${dd}`;
+
+}
+
+//convert kelvin to fahrenheit
+const convertKToF = (kelvin) => {
+  return (kelvin - 273.15) * 1.8 + 32;
+};
+
+
+
 // Function getting user's location info and fetching from the weather api
 
 const fetchWeatherData = (APIKEY, lat, long) => {
@@ -31,9 +68,9 @@ const fetchWeatherData = (APIKEY, lat, long) => {
     .then((res) => res.json())
     .then((data) => (weatherInfo = data))
     .then(() => {
-      let convertedTemp = convertKelvin(weatherInfo.main.temp);
+      let convertedTemp = convertKToF(weatherInfo.main.temp);
       temp.innerText = `${parseInt(convertedTemp)}°`;
-      dateTime.innerHTML = `${today} in ${weatherInfo.name}`;
+      dateTime.innerHTML = `${formattedDate} in ${weatherInfo.name}`;
       humidity.innerHTML = `${weatherInfo.main.humidity}%`;
       wind.innerHTML = `${parseInt(weatherInfo.wind.speed)} mph`;
       pressure.innerHTML = `${weatherInfo.main.pressure} mb`;
@@ -46,8 +83,7 @@ const getCoordsAndFetchData = () => {
   );
 };
 
-getCoordsAndFetchData();
 
-const convertKelvin = (kelvin) => {
-  return (kelvin - 273.15) * 1.8 + 32;
-};
+
+getCoordsAndFetchData();
+formatDate();
